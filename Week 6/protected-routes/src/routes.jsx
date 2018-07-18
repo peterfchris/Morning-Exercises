22552aba62198e71d6149015c73798e322d83ad8
rasbandit <todd.rasband@gamil.com>
2018-07-18 16:34:17 -0600

    fixe hashrouer location

--- Week 6/protected-routes/src/routes.jsx
+++ Week 6/protected-routes/src/routes.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route, HashRouter, Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import Dashboard from './Components/Dashboard';
 import Contact from './Components/Contact';
 import Home from './Components/Home';
@@ -11,20 +11,18 @@ export default class Routes extends Component {
 
   render() {
     return (
-      <HashRouter>
-        <main>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/dashboard">Dashboard</Link>
-            <Link to="/contact">Contact</Link>
-          </nav>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/contact" component={Contact} />
-          </Switch>
-        </main>
-      </HashRouter>
+      <main>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </main>
     );
   }
 }
